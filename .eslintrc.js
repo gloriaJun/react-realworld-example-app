2799b634ac0a248c2fc8e99a70e5c95ddc43caa3
gloriaJun <pureainu@gmail.com>
2019-04-26 00:55:21 +0900

    [#1] Set Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
   },
   parser: 'babel-eslint',
-  extends: ['airbnb'],
+  extends: ['airbnb', 'prettier'],
   plugins: ['react', 'jsx-a11y', 'import'],
   rules: {
     // "no-console": "off",
--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <header className="App-header">Hello</header>
     </div>
   );
 }
