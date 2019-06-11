69d58c568b3852eaa511168d5421a9cfa5156af1
gloriaJun <pureainu@gmail.com>
2019-06-12 01:03:02 +0900

    Add dummy file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
-  );
+  return <div>Hello</div>;
 }
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+// import { createStore, applyMiddleware } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+//
+// import sagas from './saga';
+// import reducer from './reducer';
+//
+// const sagaMiddleware = createSagaMiddleware();
+// const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+//
+// sagaMiddleware.run(sagas);
+//
+// // const action = type => store.dispatch({ type });
+//
+// // const configure = () => {
+// //   const store = createStore(reducer);
+// //   return store;
+// // };
+// //
+// // export default configure;
