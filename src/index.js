7db62ac64ea37ba262c7036e08eede6dbcba7c15
gloriaJun <pureainu@gmail.com>
2019-06-13 01:30:53 +0900

    [#3] Add Container Component for page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '@/container/app';
+import App from '@/container/app/App';
 
 const render = () => {
   ReactDOM.render(<App />, document.getElementById('root'));
