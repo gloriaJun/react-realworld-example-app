b42775ef0ee7a1490b11930a0b60eb6430110dd5
gloriaJun <pureainu@gmail.com>
2019-06-25 00:31:21 +0900

    [#4] Add the footer component

--- src/container/app/App.js
+++ src/container/app/App.js
@@ -3,6 +3,7 @@ import { HashRouter } from 'react-router-dom';
 
 import Header from 'container/app/Header';
 import Routes from 'container/app/Routes';
+import Footer from 'container/app/Footer';
 
 const { TITLE } = process.env;
 
@@ -11,6 +12,7 @@ export default () => {
     <HashRouter>
       <Header title={TITLE} />
       <Routes />
+      <Footer title={TITLE} />
     </HashRouter>
   );
 };
